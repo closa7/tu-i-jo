7895da7f9ac4a18444f5099d0bcc1b6d7500d530
closa7 <poljsparrow@gmail.com>
2025-04-17 19:00:45 +0200

    Fix: base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tu-i-jo/', // 👈 muy importante para GitHub Pages
+  base: '/tu-i-jo/', // 👈 esta línea es esencial para GitHub Pages
 })
